e2883876d6e5c717e9b8faf81fb6297bf7afd38f
Akshat Vasavada <akshatvas@gmail.com>
2015-10-04 01:12:52 -0700

    first cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -19,19 +19,4 @@ function setUser(eventId, userId){}
 // Other
 Parse.Cloud.define('hello', function(request, response) {
 	response.success("SUCCESS!");
-	// var likeQuery = new Parse.Query(LikeData);
-	// var eventQuery = new Parse.Query(EventData);
-	
-	// var eventData = eventQuery.and(request.params.userId, request.params.eventId);
-
-	// eventQuery.find({
-	// 	success: function(results) {
- //     		response.success(results);
-	// 	},
-	// 	error: function(error) {
-	// 		 // There was an error.
- //    		response.error(error);
-	// 	}
-	// });
-
 });
